e9487ef97448c77a627116485a06dd52c3222405
raulp <raulpmg.kadasoftware@gmail.com>
2023-03-24 17:07:45 -0600

    Add components and eventos

--- src/AppuseState.js
+++ src/AppuseState.js
@@ -6,13 +6,17 @@ import Container from 'react-bootstrap/Container';
 import './AppuseState.css';
 
 import { MiPrimerEstado } from './components/MiPrimerEstado';
+import { EjercicioComponente } from './components/EjercicioComponente';
 
-function AppTest() {
-	
+function AppUseState() {
+	const fecha = new Date();
+	const yearActual = fecha.getFullYear();
 	return (
 		<div className="AppuseState">
 			<header className="App-headeruseState">
 				<Container>
+					<EjercicioComponente year={yearActual}/>
+					<hr/>
 					<h3>El estado en ReactJS - Hook useState</h3>
 					<MiPrimerEstado/>
 				</Container>
@@ -21,4 +25,4 @@ function AppTest() {
 	);
 }
 
-export default AppTest;
+export default AppUseState;
