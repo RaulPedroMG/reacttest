637868672100faefae05e94808cdfcdaa8b1033d
Raulm3ga <raulpedromega@gmail.com>
2023-03-31 08:32:44 -0600

    Add Check Formulario and begin Routers

---
+++ src/AppRouters.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+
+import './AppRouters.css';
+
+
+
+function AppRouters() {
+	return (
+		<div className="AppuseState">
+			<header className="App-headerFormulariosEventos">
+				<Container>
+					
+				</Container>
+			</header>
+		</div>
+	);
+}
+
+export default AppRouters;
--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import AppUseEffect from './AppUseEffect';
 import reportWebVitals from './reportWebVitals';
 import { StyledEngineProvider } from '@mui/material/styles';
 import AppFormulariosEventos from './AppFormulariosEventos';
+import AppRouters from './AppRouters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<StyledEngineProvider injectFirst>
+			<AppRouters></AppRouters>
 			<AppFormulariosEventos />
 			<AppUseEffect />
 			<AppuseState />
